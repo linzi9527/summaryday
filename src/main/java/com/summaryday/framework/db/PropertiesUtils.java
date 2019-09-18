95d72bdabfd84ebfa64976f8b6ae5ed42154028a
nano <9527>
2019-09-18 15:37:42 +0800

    通过实际使用总结的精简单库持久层框架

--- src/main/java/com/summaryday/framework/db/PropertiesUtils.java
+++ src/main/java/com/summaryday/framework/db/PropertiesUtils.java
@@ -9,7 +9,6 @@ public class PropertiesUtils {
 	private static final Logger logger = LoggerFactory.getLogger(PropertiesUtils.class);
 	//private static ResourceBundle  BUNDLE       = ResourceBundle.getBundle("db-ms");
 	public static  ResourceBundle BUNDLE_db =null;
-	public static  ResourceBundle BUNDLE_dbms =null;
 	public static boolean Develop_Mode=false;
 	public static boolean SQL_FORMAT=false;
 	public static boolean JSON_LOG=false;
@@ -17,7 +16,7 @@ public class PropertiesUtils {
 	public static boolean Is_MS_OPEN=false;//是否开启主从配置
 	public static boolean db_Master_W=false;
 	private static String loadMessage="";
-	
+
 	static {
 		/////////////////////////单库参数配置///////////////////////////////////////
 		try {
@@ -41,41 +40,6 @@ public class PropertiesUtils {
 				logger.error("配置文件加载Develop_Mode,SQL_FORMAT异常:"+e.getMessage());
 			}
 
-		}else{
-		
-		////////////////////////////主从配置参数////////////////////////////////////////////////////////////
-		try {
-			BUNDLE_dbms = ResourceBundle.getBundle("db-ms");
-			} catch (Exception e) {
-				loadMessage+="|"+e.getMessage();
-				//logger.error("主从db-ms.properties配置文件加载异常:"+e.getMessage());
-			}
-
-		if(null!=BUNDLE_dbms){
-			logger.warn("加载Muti库配置文件[db-ms.properties], 已开起主从模式...");
-			try {
-				JSON_LOG=StringUtil.StringToBoolean(BUNDLE_dbms.getString("json_log"));
-			} catch (Exception e) {
-				logger.error("配置文件加载JSON_LOG异常:"+e.getMessage());
-			}
-			try {
-				Is_MS_OPEN =StringUtil.StringToBoolean(BUNDLE_dbms.getString("Is_MS_OPEN"));
-				db_Master_W=StringUtil.StringToBoolean(BUNDLE_dbms.getString("db_Master_W"));
-				
-			} catch (Exception e) {
-				logger.error("获取Is_MS_OPEN,db_Master_W异常："+e.getMessage());
-			}
-			try {
-					Develop_Mode=StringUtil.StringToBoolean(BUNDLE_dbms.getString("develop_mode"));
-				      SQL_FORMAT=StringUtil.StringToBoolean(BUNDLE_dbms.getString("sql_format"));
-			} catch (Exception e) {
-				logger.error("配置文件加载Develop_Mode,SQL_FORMAT异常:"+e.getMessage());
-			}
-		 }else{
-			 logger.error("加载数据库配置文件"+loadMessage);
-		 }
-		
-		
 		}
 		
 		
